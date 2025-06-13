34bc2c528d0191d4455e78e0a5aada20fe5b7dac
krzysztofdudz <u64725@student.wat.edu.pl>
2025-06-13 08:39:34 +0200

    wip: update

---
+++ utils/model.py
@@ -0,0 +1,20 @@
+chargingstation: list[dict] = [
+    {'chargingstation_name': 'Circle K', 'company_location': 'Poznań'},
+    {'chargingstation_name': 'Greenway', 'company_location': 'Kraków'},
+    {'chargingstation_name': 'EKOEN', 'company_location': 'Gdynia'},
+    {'chargingstation_name': 'Orlen', 'company_location': 'Gdańsk'}
+]
+
+clients: list[dict] = [
+    {'client_name': 'Piotr Łaski', 'client_chargingstation': 'CatandDog', 'client_location1': 'Warszawa', 'client_location2': 'Poznań'},
+    {'client_name': 'Dawid Rzeźnik', 'client_chargingstation': 'CatandDog', 'client_location1': 'Rzeszów', 'client_location2': 'Kraków'},
+    {'client_name': 'Artur Morgan', 'client_chargingstation': 'CatandDog', 'client_location1': 'Wrocław', 'client_location2': 'Gdynia'},
+    {'client_name': 'Jakub Wojciechowski', 'client_chargingstation': 'CatandDog', 'client_location1': 'Lublin', 'client_location2': 'Gdańsk'}
+]
+
+workers: list[dict] = [
+    {'worker_name': 'Kamil Pałgan', 'worker_company': 'Circle K', 'worker_location': 'Zawiercie'},
+    {'worker_name': 'Tomasz Piotrowski', 'worker_company': 'Greenway', 'worker_location': 'Warka'},
+    {'worker_name': 'Karol Zabroń', 'worker_company': 'EKOEN', 'worker_location': 'Piła'},
+    {'worker_name': 'Dominik Kowalski', 'worker_company': 'Orlen', 'worker_location': 'Słupsk'}
+]
